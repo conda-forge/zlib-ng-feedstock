618367e48143400da5556a874d57c2e495e96b86
Mark Harfouche <mark.harfouche@gmail.com>
2024-06-19 22:24:22 -0400

    exit

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -19,8 +19,7 @@ cmake --build . --config Release
 if errorlevel 1 exit 1
 
 ctest -C release
-if errorlevel 1 (ctest -C release --rerun-failed --output-on-failure && cmd /c exit 1)
+if errorlevel 1 exit 1
 
 cmake --build . --target install --config Release
 if errorlevel 1 exit 1
-
